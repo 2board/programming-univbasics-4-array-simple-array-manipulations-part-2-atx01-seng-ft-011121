549ad43ab29d8e0b4e996a0090b69b7d58017805
David Jun <djun301@gmail.com>
2021-01-02 19:19:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,6 +2,6 @@ def using_concat(array, string)
   array.concat(string)
 end
 
-def using_insert(array, element)
-  array.insert(-1, element)
+def using_insert(array, string)
+  array.insert(-1, string)
 end
